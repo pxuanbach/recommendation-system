a19dba90dab29d78d37744698028475d1bbec12f
tiendat19521345 <dat31122001@gmail.com>
2023-01-13 17:16:58 +0700

    UI-Tab-home-div-search

--- demo/frontend/src/pages/home/index.jsx
+++ demo/frontend/src/pages/home/index.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-
+import './index.css'
 const Home = () => {
-  return <div>Home page</div>;
-};
+  return ( 
+  <div className="container">
+    <div className="body">
+      <div className="div-search">
+        <h2 className="title">Welcome.</h2>
+        <h3 className="title-1">Millions of movies, TV shows and people to discover. Explore now.</h3>
+        <div className="search">
+          <input className="input" type="text" name="name" placeholder="Search for a movie, tv show, person,..." />
+          <div className="btn-search">
+            Search
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+)};
 
 export default Home;
